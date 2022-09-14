922bebad01e80113ce0497953ff6c9a1634241c0
Erixk90 <erickleonesreyes@gmail.com>
2022-09-14 15:09:09 -0500

    finish component

--- react/WhatsappButton.tsx
+++ react/WhatsappButton.tsx
@@ -1,37 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-type Props = {
-logo: string // whatsapp.png
-phone: string // 3012670718
-message: string // Estás comunicandote con Carulla por favor deja tus datos para comunicarnos contigo
-}
-const WhatsappButton = ({logo, phone, message} : Props) =>{
-    console.log(logo, phone, message)
-    return {
-        <>
-        <p>logo</p>
-        <p>phone</p>
-        <p>message</p>
-        </>
-    }
-
-    <h1> Aqui irá mi boton de whatsapp </h1>
-}
+import WhatsappButton from "./components/WhatsappButton";
 
 
-WhatsappButton.propTypes = {
-    logo: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired,
-    message: PropTypes.string.isRequired,
 
-}
-
-WhatsappButton.defaultProps = {
-    logo: "mi-logo.png",
-    phone: "3012670719",
-    message: "// Estás comunicandote con Carulla por favor deja tus datos para comunicarnos contigo"
-    
-}
-
-export default WhatsappButton
+export default WhatsappButton;
--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,5 +1,89 @@
-import WhatsappButton from "./components/WhatsappButton";
+import React from 'react';
+import PropTypes from 'prop-types'
 
+type Props = {
+    logos: string  // whatsapp.png
+    phone: string  // 3012670718
+    message: string  // Estás comunicandote con Carulla por favor deja tus datos para comunicarnos contigo
+    width: number //80px
+    height: number  //80px
+}
+
+const WhatsappButton = ({ logos, phone, message, width, height }: Props) => {
+        const formattedMessage = message.replace(/ /g, "%20")
+        return(
+        <>
+        <div className='fixed bottom-2 right-2 flex flexColumn'>
+            <a 
+                href={`https://wa.me/${phone}?text=${formattedMessage}`}
+                target="_blank"
+                rel="noreferrer noopener"
+            >
+                    <img
+                        src={logos}
+                        width={width}
+                        height={height}
+                        alt="aqui deberia ir el logo" 
+                    />
+            </a>
+
+        </div>
+        </>
+    )}
+
+    WhatsappButton.propTypes = {
+        logos: PropTypes.string.isRequired,
+        phone: PropTypes.string.isRequired,
+        message: PropTypes.string,
+        width: PropTypes.number,
+        height: PropTypes.number
+    }
+
+    WhatsappButton.defaultProps = {
+        logos: "logowpp.png",
+        phone: "573012670719",
+        message: "Estás comunicandote con Carulla por favor deja tus datos para comunicarnos contigo",
+        width: 80,
+        height: 80
+
+    }
+    WhatsappButton.schema = {
+        title: "Botón de Whatsapp",
+        type: "object",
+        properties: {
+            logos:{
+                title: "Logo de Whatsapp relacionado a la marca",
+                type:"string",
+                widget:{
+                    "ui:widget":"image-uploader"
+                }
+            },
+            phone:{
+                title: "Teléfono",
+                description: "Agrega por favor tu número de teléfono",
+                type:"string"
+            },
+            message: {
+                title: "Mensaje",
+                description: "Agrega por favor el mensaje que se verá para tu cliente",
+                type:"string",
+                widget:{
+                    "ui:widget":"textarea"
+                }
+            },
+            width: {
+                title: "Teléfono",
+                description: "Agrega por favor el número de teléfono",
+                type: "number"
+    },
+            height: {
+                title: "Teléfono",
+                description: "Agrega por favor el número de teléfono",
+                type: "number"
+    }
+        }
+
+    }
 
 
 export default WhatsappButton
